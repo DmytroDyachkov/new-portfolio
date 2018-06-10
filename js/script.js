935d1c293b32a061005dd2c92fa591834c9fbfa6
DmytroDyachkov <dima2001944361@yandex.ru>
2018-06-11 00:59:25 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function FunctionHfsdf() {
+    var persd = document.getElementById("myTopnav");
+    if (persd.className === "topnav"){
+        persd.className += " responsive";
+    }else{
+        persd.className = "topnav";
+    }
+}
